4ba36e4dbc14afba4e5fb6298a0ba2ac50efee6c
payaldevgade <payaldevgade13@gmail.com>
2026-08-19 21:30:07 +0530

    Add files via upload

---
+++ day67_GinortS.py
@@ -0,0 +1,10 @@
+s = input("Enter the string: ")
+
+lower = sorted(c for c in s if c.islower())
+upper = sorted(c for c in s if c.isupper())
+odd = sorted(c for c in s if c.isdigit() and int(c) % 2 != 0)
+even = sorted(c for c in s if c.isdigit() and int(c) % 2 == 0)
+
+result = ''.join(lower + upper + odd + even)
+
+print("Sorted string:", result)
