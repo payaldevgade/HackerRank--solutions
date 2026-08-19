7d3322f2520d8831baa24ea90f53b3b7a068afb3
payaldevgade <payaldevgade13@gmail.com>
2026-08-19 21:22:05 +0530

    Add files via upload

---
+++ day62_zipped.py
@@ -0,0 +1,16 @@
+n, x = map(int, input("Enter number of students and subjects: ").split())
+
+marks = []
+
+for _ in range(x):
+    subject_marks = list(
+        map(float, input("Enter marks for subject: ").split())
+    )
+    marks.append(subject_marks)
+
+print("Average marks:")
+
+for student_marks in zip(*marks):
+    average = sum(student_marks) / x
+    print(average)
+    
