c66f223a57bddc03b40a284f3a5920c3efa59c3c
payaldevgade <payaldevgade13@gmail.com>
2026-08-20 22:48:00 +0530

    Add files via upload

---
+++ day74_sum-and-prod.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+n, m = map(int, input("Enter rows and columns: ").split())
+
+arr = []
+
+for _ in range(n):
+    row = list(map(int, input("Enter row: ").split()))
+    arr.append(row)
+
+arr = np.array(arr)
+
+sum_result = np.sum(arr, axis=0)
+product_result = np.prod(sum_result)
+
+print("Sum along axis 0:", sum_result)
+print("Product:", product_result)
