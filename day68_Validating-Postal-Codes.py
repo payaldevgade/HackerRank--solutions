5b1fd994628cc3f46cc1a7960269b5318df7f4a0
payaldevgade <payaldevgade13@gmail.com>
2026-08-20 22:43:21 +0530

    Add files via upload

---
+++ day68_Validating-Postal-Codes.py
@@ -0,0 +1,13 @@
+import re
+
+regex_integer_in_range = r"^[1-9][0-9]{5}$"
+regex_alternating_repetitive_digit_pair = r"(\d)(?=\d\1)"
+
+P = input("Enter postal code: ")
+
+result = (
+    bool(re.match(regex_integer_in_range, P))
+    and len(re.findall(regex_alternating_repetitive_digit_pair, P)) < 2
+)
+
+print("Valid postal code:", result)
