060742064467a29b83bb95d46954ef36efc41ece
payaldevgade <payaldevgade13@gmail.com>
2026-08-27 22:34:22 +0530

    Fix formatting and output of GinortS script

--- day67_GinortS.py
+++ day67_GinortS.py
@@ -1,5 +1,6 @@
 s = input("Enter the string: ")
 
+
 lower = sorted(c for c in s if c.islower())
 upper = sorted(c for c in s if c.isupper())
 odd = sorted(c for c in s if c.isdigit() and int(c) % 2 != 0)
