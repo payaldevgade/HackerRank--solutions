22c761f21b21036c3f4f1081286f507184aa48ec
payaldevgade <payaldevgade13@gmail.com>
2026-08-27 22:34:37 +0530

    Refactor input and sorting logic in GinortS.py

--- day67_GinortS.py
+++ day67_GinortS.py
@@ -6,6 +6,7 @@ upper = sorted(c for c in s if c.isupper())
 odd = sorted(c for c in s if c.isdigit() and int(c) % 2 != 0)
 even = sorted(c for c in s if c.isdigit() and int(c) % 2 == 0)
 
+
 result = ''.join(lower + upper + odd + even)
 
 print("Sorted string:", result)
