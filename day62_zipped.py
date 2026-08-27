f873379755bc39cfb3208eb36d1a2911060185e8
payaldevgade <payaldevgade13@gmail.com>
2026-08-27 22:28:32 +0530

    Update day62_zipped.py

--- day62_zipped.py
+++ day62_zipped.py
@@ -3,6 +3,7 @@ n, x = map(int, input("Enter number of students and subjects: ").split())
 marks = []
 
 for _ in range(x):
+    
     subject_marks = list(
         map(float, input("Enter marks for subject: ").split())
     )
