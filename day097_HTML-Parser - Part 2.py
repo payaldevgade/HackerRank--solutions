f1e8043de5575891d747587b61c802dce80ea906
payaldevgade <payaldevgade13@gmail.com>
2026-09-08 19:36:23 +0530

    Update day097_HTML-Parser - Part 2.py

--- day097_HTML-Parser - Part 2.py
+++ day097_HTML-Parser - Part 2.py
@@ -3,6 +3,7 @@ from html.parser import HTMLParser
 class MyHTMLParser(HTMLParser):
 
     def handle_comment(self, data):
+        
         if '\n' in data:
             print(">>> Multi-line Comment")
         else:
