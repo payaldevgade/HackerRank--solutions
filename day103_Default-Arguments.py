1dece5f9e5d00390eaba912562abb90d713e41fa
payaldevgade <payaldevgade13@gmail.com>
2026-09-08 19:32:46 +0530

    Add files via upload

---
+++ day103_Default-Arguments.py
@@ -0,0 +1,40 @@
+
+class EvenStream(object):
+    def __init__(self):
+        self.current = 0
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+
+class OddStream(object):
+    def __init__(self):
+        self.current = 1
+
+    def get_next(self):
+        to_return = self.current
+        self.current += 2
+        return to_return
+
+
+def print_from_stream(n, stream=None):
+    if stream is None:
+        stream = EvenStream()
+
+    for _ in range(n):
+        print(stream.get_next())
+
+
+q = int(input("Enter number of queries: "))
+
+for _ in range(q):
+    stream_name, n = input("Enter stream and number: ").split()
+    n = int(n)
+
+    if stream_name == "even":
+        print_from_stream(n)
+    else:
+        print_from_stream(n, OddStream())
+
