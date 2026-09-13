8630f03bdbe9468040ddbcd4e75ae587c68db379
Payal Devgade <payaldevgade13@gmail.com>
2026-09-13 21:41:30 +0530

    Update day058_check-subset.py

--- day058_check-subset.py
+++ day058_check-subset.py
@@ -2,6 +2,7 @@ T = int(input("Enter number of test cases: "))
 
 
 for _ in range(T):
+    
     n = int(input("Enter number of elements in A: "))
     A = set(map(int, input("Enter elements of A: ").split()))
 
