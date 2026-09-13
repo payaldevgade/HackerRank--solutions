53b8e4750a676a213fb324d66b2018d46a7df19c
Payal Devgade <payaldevgade13@gmail.com>
2026-09-13 21:41:40 +0530

    Remove unnecessary blank lines in subset check

--- day058_check-subset.py
+++ day058_check-subset.py
@@ -5,6 +5,7 @@ for _ in range(T):
     
     n = int(input("Enter number of elements in A: "))
     A = set(map(int, input("Enter elements of A: ").split()))
+    
 
     m = int(input("Enter number of elements in B: "))
     B = set(map(int, input("Enter elements of B: ").split()))
