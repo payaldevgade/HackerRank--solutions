e9aedb10a10e1b2ab88af06dcb4af3536569a636
Payal Devgade <payaldevgade13@gmail.com>
2026-09-13 16:34:54 +0530

    Update day021_designer-door-mat.py

--- day021_designer-door-mat.py
+++ day021_designer-door-mat.py
@@ -13,6 +13,7 @@ for i in range(N // 2):
 print("WELCOME".center(M, "-"))
 
 # Bottom half
+
 for i in range(N // 2 - 1, -1, -1):
     pattern = ".|." * (2 * i + 1)
     print(pattern.center(M, "-"))
