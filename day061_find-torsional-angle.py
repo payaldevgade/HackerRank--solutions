572b4cdebd7ac425ae1bb3ab706d9815f2b0fb68
Payal Devgade <payaldevgade13@gmail.com>
2026-09-15 20:41:42 +0530

    Update day061_find-torsional-angle.py

--- day061_find-torsional-angle.py
+++ day061_find-torsional-angle.py
@@ -45,6 +45,7 @@ C = Points(*map(float, input("Enter point C (x y z): ").split()))
 D = Points(*map(float, input("Enter point D (x y z): ").split()))
 
 # Create vectors
+
 AB = B - A
 BC = C - B
 CD = D - C
