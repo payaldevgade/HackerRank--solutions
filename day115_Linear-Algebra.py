360059e729756f985623a92de46b207fd3e22758
Payal Devgade <payaldevgade13@gmail.com>
2026-09-16 19:48:45 +0530

    Update day115_Linear-Algebra.py

--- day115_Linear-Algebra.py
+++ day115_Linear-Algebra.py
@@ -6,8 +6,7 @@ matrix = []
 
 for i in range(n):
     
-    
-    row = list(map(float, input(f"Enter row {i + 1}: ").split()))
+     row = list(map(float, input(f"Enter row {i + 1}: ").split()))
     matrix.append(row)
     
 determinant = np.linalg.det(matrix)
