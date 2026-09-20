22ee77fcd5b7b892a22a3639b12899d6612b09f9
Payal Devgade <payaldevgade13@gmail.com>
2026-09-20 21:27:55 +0530

    Update day026_itertools-with-replacement.py

--- day026_itertools-with-replacement.py
+++ day026_itertools-with-replacement.py
@@ -5,6 +5,6 @@ k = int(k)
 
 s = "".join(sorted(s))
 
-# Generate and print combinations with 
+# Generate and print combinations 
 for i in combinations_with_replacement(s, k):
     print("".join(i))
