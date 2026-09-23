a50a78a4be96ab3d29810a32c2947f89cba90a44
Payal Devgade <payaldevgade13@gmail.com>
2026-09-23 17:33:37 +0530

    Update day054_set.difference()operation.py

--- day054_set.difference()operation.py
+++ day054_set.difference()operation.py
@@ -3,10 +3,6 @@ n = int(input("Enter number of English subscribers: "))
 
 
 
-
-
-
-
 english = set(map(int, input("Enter English roll numbers: ").split()))
 
 m = int(input("Enter number of French subscribers: "))
