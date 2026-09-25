c20f52ee3a433575fcb3df81bc572525f54a5431
Payal Devgade <payaldevgade13@gmail.com>
2026-09-25 20:56:44 +0530

    Update day014_find-a-string.py

--- day014_find-a-string.py
+++ day014_find-a-string.py
@@ -13,6 +13,7 @@ def count_substring(string, sub_string):
 if __name__ == '__main__':
     string = input("Enter the original string: ").strip()
     sub_string = input("Enter the substring to count: ").strip()
+    
 
     count = count_substring(string, sub_string)
     print(count)
